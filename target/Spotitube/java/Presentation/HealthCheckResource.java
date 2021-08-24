77b1c06d4980a43c7da66dd693105582a2cdb8e6
Wouter Smit <heiligekip@hotmail.com>
2021-08-24 13:10:13 +0200

    Setup Init

---
+++ target/Spotitube/java/Presentation/HealthCheckResource.java
@@ -0,0 +1,13 @@
+package Presentation;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/health")
+public class HealthCheckResource {
+
+    @GET
+    public String healthy() {
+        return "Up & Running";
+    }
+}
